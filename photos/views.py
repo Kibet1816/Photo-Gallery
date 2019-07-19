f488bcd8208715e1d4a7d056f507d3b5597bc208
Denis Kibet <kibet1816@gmail.com>
2019-07-19 12:39:08 +0300

    Add url mapping to render index page

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    View function to render the landing page
+    """
+    return render (request,'index.html')
