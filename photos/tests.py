138595a7622f079355191b3558f028c2b63e195e
Denis Kibet <kibet1816@gmail.com>
2019-07-19 16:02:23 +0300

    Install document formatter

--- photos/tests.py
+++ photos/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
-from .models import Image,Category,Location
+from .models import Image, Category, Location
 
 # Create your tests here.
+
+
 class ImageTestClass(TestCase):
     """
     Test class for image module
@@ -11,13 +13,26 @@ class ImageTestClass(TestCase):
         """
         Method to create an instance of the image before each test
         """
+        self.kite = Location(name="Kitengela")
+        self.kite.save_location()
 
+        self.travel = Category(name='Travel')
+        self.travel.save_category()
 
-    # def test_instance(self):
-    #     """
-    #     Test method to check for correct instantiation
-    #     """
-    #     self.assertTrue(isinstance(self.new_image,Image))
+        self.new_image = Image(image_url='image.jpg', name='Gorgon',
+                               description='Son of skrygon', location=self.kite, category=self.travel)
+        self.new_image.save()
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+
+    def test_instance(self):
+        """
+        Test method to check for correct instantiation
+        """
+        self.assertTrue(isinstance(self.new_image, Image))
 
     def test_save_method(self):
         """
@@ -29,17 +44,18 @@ class CategoryTestClass(TestCase):
     """
     Test class for category module
     """
+
     def setUp(self):
         """
         Method to create an instance of the image before each test
         """
-        self.new_category = Category(name = 'Travel')
+        self.new_category = Category(name='Travel')
 
     def test_instance(self):
         """
         Test method to check for correct instantiation
         """
-        self.assertTrue(isinstance(self.new_category,Category))
+        self.assertTrue(isinstance(self.new_category, Category))
 
     def test_save_method(self):
         """
@@ -47,25 +63,27 @@ class CategoryTestClass(TestCase):
         """
         self.new_category.save_category()
         categories = Category.objects.all()
-        self.assertTrue(len(categories)>0)
+        self.assertTrue(len(categories) > 0)
+
 
 class LocationTestClass(TestCase):
     """
     Test class for location module
     """
+
     def setUp(self):
         """
         Method to create an instance of the image before each test
         """
-        self.new_location = Location(name = 'Kite')
+        self.new_location = Location(name='Kite')
 
     def test_instance(self):
         """
         Test method to check for correct instantiation
         """
-        self.assertTrue(isinstance(self.new_location,Location))
+        self.assertTrue(isinstance(self.new_location, Location))
 
     def test_save_method(self):
         self.new_location.save_location()
         locations = Location.objects.all()
-        self.assertTrue(len(locations)>0)
+        self.assertTrue(len(locations) > 0)
