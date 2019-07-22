82aa633b3584f7e5524bd9e23f63e16dfcfb255b
Denis Kibet <kibet1816@gmail.com>
2019-07-22 07:34:19 +0300

    Register additional classes as admin site

--- photos/admin.py
+++ photos/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Image,Category,Location
+from .models import Image,Category,Location,Landscapes,Travel,Sport,Animals,Architecture
 
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Category)
 admin.site.register(Location)
+admin.site.register(Travel)
+admin.site.register(Sport)
+admin.site.register(Landscape)
+admin.site.register(Food)
+admin.site.register(Animals)
+admin.site.register(Architecture)
